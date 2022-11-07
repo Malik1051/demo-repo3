23f566b7ef0c6a401d3cba07e4bc5891f099905c
Ben Malik <benmaleek271@gmail.com>
2022-11-07 06:18:44 +0000

    PRACTICING

---
+++ myfirstprogram.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void myFunction() {
+    char fullName [30];
+    printf("what is your name?:\n");
+    scanf("%s", fullName);
+    printf("hello %s nice to meet you",fullName);
+
+}
+
+int main(){
+    myFunction();
+
+
+
+
+
+    return 0;
+}
