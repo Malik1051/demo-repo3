3ceb09481babaff0e61b9ed66e99c59d24579917
Ben Malik <benmaleek271@gmail.com>
2022-11-09 07:41:27 +0000

    new

--- myfirstprogram.c
+++ myfirstprogram.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 
+
 void myFunction() {
     char fullName [30];
     printf("what is your name?:\n");
     scanf("%s", fullName);
     printf("hello %s nice to meet you",fullName);
+    
 
 }
 
 int main(){
     myFunction();
+    int var1;
+    char var2 [10];
+    printf("Address of var1 variable:%x\n", &var1);
+    printf("Address of var2 variable :%x\n", &var2);
 
 
 
